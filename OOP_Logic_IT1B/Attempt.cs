93b7e88ea87d02f109717b0fda047af36aaadd3c
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2022-05-31 11:38:36 +0200

    po hodině

---
+++ OOP_Logic_IT1B/Attempt.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Logic_IT1B
+{
+    public class Attempt
+    {
+        public Pin[] Pins { get; internal set; }
+
+        public Attempt()
+        {
+            Pins = new Pin[4];
+        }
+
+        public List<bool> Evaluate(Pin[] pattern)
+        {
+            List<bool> result = new List<bool>();
+            bool[] flag = new bool[4];
+            
+            
+            return new List<bool>();
+        }
+    }
+}
