e4f384ea2d65df6c6e487cda025c48364d4c1c6e
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2022-05-31 11:08:05 +0200

    Přidejte soubory projektu.

---
+++ OOP_Logic_IT1B/Pin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace OOP_Logic_IT1B
+{
+    public class Pin
+    {
+        public Color Color { get; set; }
+    }
+
+    public static class PinColor
+    {
+        public static Color Color1 = Colors.Red;
+        public static Color Color2 = Colors.Green;
+        public static Color Color3 = Colors.Blue;
+        public static Color Color4 = Colors.Orange;
+        public static Color Color5 = Colors.Yellow;
+        public static Color Color6 = Colors.Purple;
+    }
+}
